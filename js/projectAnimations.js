00d842598b23e05419253c4229055b5e1357bd51
Eric <Ericrius1@gmail.com>
2014-04-25 14:28:25 -0700

    Got panels rotating BOOSH

--- js/projectAnimations.js
+++ js/projectAnimations.js
@@ -1,18 +1,27 @@
 function handleProjectRotation(frame, projCategory){
   var rotationTime = 3000; 
+  var hasSwapped = false;
   //Set up the tween rotation
   var curRot = {y : frame.rotation.y};
   var targetRot = {y: frame.rotation.y + 2 * Math.PI};
   var projTween = new TWEEN.Tween(curRot).
     to(targetRot, rotationTime).
     easing(TWEEN.Easing.Cubic.InOut).
-    // repeat(1).
-    // yoyo(true).
     onUpdate(function(){
       frame.rotation.y = curRot.y;
+      if(Math.abs(targetRot.y/2 - curRot.y) < 3 && !hasSwapped){
+        swapPanels();
+        hasSwapped = true;
+      }
     }).start();
-    projTween.onComplete(function(){
-      //switch out panel for the next one in the projectsMap array
+
+    setTimeout(function(){
+      handleProjectRotation(frame, projCategory);
+    }, 7000)
+
+    var swapPanels = function(){
+      console.log("WAHHH");
+       //switch out panel for the next one in the projectsMap array
       frame.children[0].html.style.display = "none";
       frame.remove(frame.children[0]);
       var projObj = projectsMap[projCategory];
@@ -26,6 +35,8 @@ function handleProjectRotation(frame, projCategory){
       panels[newPanelName].html.style.display = 'block';
       panels[newPanelName].html.style.opacity = 1;
       frame.add(panels[newPanelName]);
-    });
+    };
 }
+
+
 
