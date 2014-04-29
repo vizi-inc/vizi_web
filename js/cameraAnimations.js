d1fa08063cd60368f5e84df358b58339a450139f
Eric <Ericrius1@gmail.com>
2014-04-29 15:33:22 -0700

    set caam pos

--- js/cameraAnimations.js
+++ js/cameraAnimations.js
@@ -1,32 +1,54 @@
+var camPosIndex = 0;
 function rotateCamera(){
   var direction = Math.random() > 0.5 ? 1 : -1;
   var theta = Math.PI * 0.5 *  direction;
   var x = camera.position.x;
   var z = camera.position.z;
   // {_x: -2.414950320276892, _y: 0.4561987430265692, _z: 2.768357218931255,
-  // var camPositions = [
-  //   {
-  //     x: 
-  //     y:
-  //     z: 
+  var camPositions = [
+    {
+      x: -50.4,
+      y: 88,
+      z: -107
 
-  //   },
-  //   {
+    },
+    {
+      x: 80,
+      y: 69,
+      z: -101
+    },
+    {
+      x: 72,
+      y: 54,
+      z: 113
+    },
+    {
+      x: -60,
+      y: 60,
+      z: 118
+    }
 
-  //   }
-
-  // ]
+  ];
 
   var currentPos = {
     x: x, 
     y: camera.position.y,
     z: z
   };
+  if(camPosIndex > camPositions.length-1){
+    camPosIndex = 0;
+  }
+  var curCamPos = camPositions[camPosIndex++];
+  // var finalPos = {
+  //   x: x * Math.cos(theta) + z * Math.sin(theta),
+  //   y: camera.position.y,
+  //   z: z * Math.cos(theta) - x * Math.sin(theta)
+  // };
 
   var finalPos = {
-    x: x * Math.cos(theta) + z * Math.sin(theta),
-    y: camera.position.y,
-    z: z * Math.cos(theta) - x * Math.sin(theta)
+    x: curCamPos.x,
+    y: curCamPos.y,
+    z: curCamPos.z
   };
 
 
